2c3d3945b89532db0b2e7e192d3fe97b2fa19773
Vanessa Flores <risingdevhabits@gmail.com>
2025-02-13 10:28:27 -0600

    Organize files

--- Pick Your Protien/ContentView.swift
+++ Pick Your Protien/ContentView.swift
@@ -32,37 +32,7 @@ struct ContentView: View {
     ContentView()
 }
 
-let animalProteinCategories: [String] = ["Beef", "Pork", "Poultry", "Specialty Meats", "Seafood"]
 
-let beefProteinOptions: [String] = ["Ground Beef",
-                                    "Sirloin Tips",
-                                    "Burger",
-                                    "Ground Sirloin",
-                                    "Flank Steak"]
-
-let porkProteinOptions: [String] = ["Ground Pork",
-                                    "St. Louis Ribs",
-                                    "Baby Back Ribs",
-                                    "Italian Sausage Links",
-                                    "Boneless Pork Chops",
-                                    "Pork Tenderloin",
-                                    "Breakfast Sausage",
-                                    "Bacon"]
-
-let poultryProteinOptions: [String] = ["Chicken Meatballs",
-                                       "Boneless Skinless Chicken Breasts",
-                                       "Boneless Chicken Thighs",
-                                       "Chicken Drumsticks",
-                                       "Bone-in Chicken Thighs",
-                                       "Chicken Wings",
-                                       "Turkey Bacon",
-                                       "Ground Turkey",
-                                       "Ground Chicken",
-                                       "Turkey Breast"]
-
-let seafoodProteinOptions: [String] = ["Shrimp", "Atlantic Salmon", "Sockeye Salmon", "Sea Scallops", "Cod", "Gulf Shrimp"]
-
-let specialtyMeatProteinOptions: [String] = ["Ground Lamb", "Ground Bison"]
 
 struct CategoryPill: View {
     let title: String
